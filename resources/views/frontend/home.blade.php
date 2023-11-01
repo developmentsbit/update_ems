5e8a3211750694c18a50e750e30797851f07e305
Tajim Islam <tjm4181@gmail.com>
2023-11-01 15:35:32 +0600

    update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -296,7 +296,7 @@ $setting = DB::table("setting")->first();
 						@endphp
 
 						<div class="col-12 mt-4">
-							<iframe width="100%" height="400" src="{{ $setting->youtube }}" title="METROPOLITAN SCHOOL & COLLEGE Documentary." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+							<iframe width="100%" height="400" src="{{ $setting->youtube }}" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 						</div>
 
 						<!-------------------------------------End Full Card---------------------------------->

f94025bdd45cc79255469f7bf5bcbe3c9fb5cd62
ontor07 <110522352+ontor07@users.noreply.github.com>
2023-12-24 13:45:54 +0600

    cash_tran

--- app/Models/cash_transaction.php
+++ app/Models/cash_transaction.php
@@ -6,14 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class cash_deposit extends Model
+class cash_transaction extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [];
-
-    public function receiver()
-    {
-        return $this->belongsTo('App\Models\cash_receiver_info','receiver_id');
-    }
 }

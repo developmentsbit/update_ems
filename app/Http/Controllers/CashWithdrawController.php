9438982e652b9df49160a6ecbf3b84e6f73b4890
ontor07 <110522352+ontor07@users.noreply.github.com>
2023-12-24 13:36:41 +0600

    cash

---
+++ app/Http/Controllers/CashWithdrawController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\cash_receiver_info;
+use App\Models\cash_withdraw;
+use Brian2694\Toastr\Facades\Toastr;
+
+
+class CashWithdrawController extends Controller
+{
+    public function getDate($sign,$value)
+    {
+        $explode = explode($sign,$value);
+
+        $date = $explode[2].'-'.$explode[0].'-'.$explode[1];
+
+        return $date;
+    }
+    /**
+     * 
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

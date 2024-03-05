e3bd953b42ed2f2855c6e5bd3cff587b660017bf
Tajim Islam <tjm4181@gmail.com>
2024-03-05 11:41:50 +0600

    'seeder_error_solve'

--- database/seeders/UpazilaInformationsTableSeeder.php
+++ database/seeders/UpazilaInformationsTableSeeder.php
@@ -14,7 +14,7 @@ class UpazilaInformationsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        \DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 
         \DB::table('upazila_informations')->delete();
         
